89e3a7437725b439775c9397350f964c3780215d
MOH3N <mohsenir@proton.me>
2025-03-28 07:19:44 +0330

    add copy arrays func

--- main.c
+++ main.c
@@ -9,7 +9,20 @@ void main()
 }
 
 
-void cpy_arr(uint32_t *arr,uint8_t len)
+uint32_t* cpy_arr(uint32_t *arr,int8_t len)
 {
-    
+    uint32_t *ptr;
+    ptr = (void*)malloc(len*sizeof(arr));
+
+    if(ptr==NULL)
+    {
+        exit(0);
+    }
+    else
+    {
+        for(int8_t k=0; k < len ;k++)
+            ptr[k]=arr[k];
+    }
+
+
 }
