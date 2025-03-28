a65d76c58835059bd3972dd9bfe55af75ecb4679
MOH3N <mohsenir@proton.me>
2025-03-28 07:47:20 +0330

    add final code to project

--- main.c
+++ main.c
@@ -2,27 +2,24 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+uint32_t* cpy_arr(uint32_t *arr,int8_t len);
 
 void main()
 {
+    uint32_t a[]={1,5,6,7,8,9,10};
+    uint32_t *b;
+    b = cpy_arr(a,7);
 
+    for(int i=0;i<7;i++) printf("%3d",b[i]);
+  
 }
-
 
 uint32_t* cpy_arr(uint32_t *arr,int8_t len)
 {
     uint32_t *ptr;
-    ptr = (void*)malloc(len*sizeof(arr));
+    ptr = (void*)malloc(len*sizeof(uint32_t));
 
-    if(ptr==NULL)
-    {
-        exit(0);
-    }
-    else
-    {
-        for(int8_t k=0; k < len ;k++)
-            ptr[k]=arr[k];
-    }
-
-
+    for(int8_t k=0; k < len ;k++) ptr[k]=arr[k];
+    
+    return ptr;
 }
